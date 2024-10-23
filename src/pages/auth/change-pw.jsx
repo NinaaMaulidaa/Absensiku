db1207ade6bdc02094c2d0a29edbc01a4b388165
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-10-23 16:51:05 +0800

    fix change password

--- src/pages/auth/change-pw.jsx
+++ src/pages/auth/change-pw.jsx
@@ -3,8 +3,36 @@ import {
   Button,
   Typography,
 } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 export function ChangePw() {
+  const navigate = useNavigate()
+  const changePassword = async (e) => {
+    e.preventDefault()
+    const change = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    }
+    try {
+      await axios.put(`https://88gzhtq3-8000.asse.devtunnels.ms/api/v1/auth/change-password`, change, {})
+      Swal.fire({
+        title: "Berhasil!",
+        text: "Ganti Password berhasil di lakukan!.",
+        icon: "success",
+        timer: 2000
+      });
+      navigate('/auth/sign-in')
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        title: "Gagal!",
+        text: error,
+        icon: "error",
+      });
+    }
+  }
   return (
     <section className="h-screen gap-4 flex">
             <div className="w-2/5 h-full hidden lg:block">
@@ -24,13 +52,14 @@ export function ChangePw() {
           {/* <Typography variant="h2" className="font-bold my-4">Change Password</Typography> */}
           <Typography variant="paragraph" color="blue-gray" className="lg:text-lg text-sm font-normal">Enter your email and password to Change Password.</Typography>
         </div>
-        <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
+        <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2" onSubmit={changePassword}>
           <div className="mb-1 flex flex-col gap-6">
             <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
               Your email
             </Typography>
             <Input
               size="lg"
+              name="email"
               placeholder="name@mail.com"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
@@ -43,6 +72,7 @@ export function ChangePw() {
             <Input
               type="password"
               size="lg"
+              name="password"
               placeholder="********"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900 "
               labelProps={{
@@ -51,11 +81,9 @@ export function ChangePw() {
             />
           </div>
           <div>
-          <a href="/auth/sign-in">
-          <Button className="mt-6" fullWidth>
-          Sumbit
+          <Button type="submit" className="mt-6" fullWidth>
+          Submit
           </Button>
-          </a>
           </div>
         </form>
 
